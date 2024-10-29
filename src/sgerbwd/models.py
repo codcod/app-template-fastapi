260d741868da5a0395b6abb8f02f593b6e07f919
codcod <nikos@linux.com>
2024-10-29 22:17:32 +0100

    feat: add mechanism for alembic migrations

--- src/sgerbwd/models.py
+++ src/sgerbwd/models.py
@@ -1,5 +1,16 @@
 from sqlmodel import Field, SQLModel
 
+NAMING_CONVENTION = {
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(constraint_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s',
+}
+
+metadata = SQLModel.metadata
+metadata.naming_convention = NAMING_CONVENTION
+
 
 class User(SQLModel, table=True):
     user_id: int | None = Field(default=None, primary_key=True)
